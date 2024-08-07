f1781a880f8caed7eae8be73830ef1a1d9b7d446
bar-schtalman <barschtalman@gmail.com>
2024-08-07 16:55:52 +0300

    with FPS

--- src/main/java/com/handson/basic/repo/StudentService.java
+++ src/main/java/com/handson/basic/repo/StudentService.java
@@ -4,6 +4,7 @@ import com.handson.basic.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,5 +29,7 @@ public class StudentService {
     public void delete(Student student) {
         repository.delete(student);
     }
-
+    public List<Student> getStudentWithSatHigherThan(Integer sat) {
+        return repository.findAllBySatScoreGreaterThan(sat);
+    }
 }
